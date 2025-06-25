f27742a62348359e729d78d699d02aa80b764759
Ethan <neh9483@uncw.edu>
2025-06-25 14:29:46 -0400

    repo name change

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/skate-test/",
+  base: "/skate/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
